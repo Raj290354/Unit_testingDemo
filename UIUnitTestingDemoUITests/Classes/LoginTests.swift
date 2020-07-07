0d93ec7e9c628645f8693fee921365b9a1aef27c
Rakesh kumar <rkuma@seasiainfotech.com>
2020-07-07 14:57:43 +0530

    commit

--- UIUnitTestingDemoUITests/Classes/LoginTests.swift
+++ UIUnitTestingDemoUITests/Classes/LoginTests.swift
@@ -14,7 +14,7 @@ class LoginTests: BaseTests {
 
     override func setUp() {
         super.setUp()
-        
+        //rakesh
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
